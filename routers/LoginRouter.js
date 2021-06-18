dd09eaaae9a3196e6d38f02038f455f859e66552
sohila-mohamed <sohailamohamed.sbme@gmail.com>
2021-06-19 01:52:23 +0200

    adding async middleware for templating try and catch for each controller callback

--- routers/LoginRouter.js
+++ routers/LoginRouter.js
@@ -3,15 +3,13 @@ const Login_Router = express.Router();
 // const multer = require('multer');
 // const { Authenticate } = require('../middlewares/Auth');
 const { LoginUser } = require('../controllers/LoginController');
-// const { AsyncMiddleware } = require('../middlewares/async');
+const { AsyncMiddleware } = require('../middlewares/Async');
 // const { upload } = require('../middlewares/upload');
 //uploading file configurations 
 
 
 
 ///APIs
-
-
-Login_Router.post('/log', LoginUser);
+Login_Router.post('/log', AsyncMiddleware(LoginUser));
 
 module.exports = Login_Router;
--- routers/RegisterationRouter.js
+++ routers/RegisterationRouter.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const Registration_Router = express.Router();
 const { AddNewUser } = require('../controllers/RegistrationController');
+const { AsyncMiddleware } = require('../middlewares/Async');
+
 
 ///APIs
-Registration_Router.post('/', AddNewUser);
+Registration_Router.post('/', AsyncMiddleware(AddNewUser));
 module.exports = Registration_Router;
