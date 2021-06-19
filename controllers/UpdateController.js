e75d1d9e81faf402ae6be9a806925a67bef20704
sohila-mohamed <sohailamohamed.sbme@gmail.com>
2021-06-19 03:28:19 +0200

    handling monogodb duplicate key error with update request

--- controllers/UpdateController.js
+++ controllers/UpdateController.js
@@ -3,19 +3,31 @@ async function UpdateUserData(req, res, next) {
     if (req.user._id !== req.params.id) return next({ message: "Un-Authorized request", status: 401 });
 
     //check if already registered 
-    let user = await req.DB_Scheme.User.findOne({ email: req.body.email });
+    let user = await req.DB_Scheme.User.findOne({ _id: req.params.id });
     if (!user) return next({ status: 400, message: 'Not registered yet' });
 
     let result = await req.DB_Scheme.User.updateOne({ _id: req.params.id }, {
         $set: {
             firstName: req.body.firstName,
             lastName: req.body.lastName,
-            email: req.body.email,
+            email: req.body.email === user.email ? user.email : req.body.email,
             profileImg: req.file.filename ? req.file.filename : user.profileImg,
             password: req.body.password ? req.body.password : user.password,
             plan: req.body.plan ? req.body.plan : user.plan
         }
-    })
+    }).catch((err) => {
+        console.log("update error");
+        if (err.name === 'MongoError' && err.code === 11000) {
+            if (err.keyPattern.hasOwnProperty('email'))
+                next({ status: 422, message: ` invalid email`, err: err })
+            else if (err.keyPattern.hasOwnProperty('password'))
+                next({ status: 422, message: ` invalid password`, err: err })
+            else
+                next({ status: 422, message: ` invalid update`, err: err })
+        }
+
+
+    });
     if (!result) return next({ status: 400, message: "Bad Request" });
     return res.send(result);
 
--- middlewares/Async.js
+++ middlewares/Async.js
@@ -3,6 +3,7 @@ function AsyncMiddleware(handler) {
         try {
             await handler(req, res, next);
         } catch (err) {
+            console.log("error type", typeof(err))
             next({ status: 505, message: "internal server error", err: err });
 
         }
